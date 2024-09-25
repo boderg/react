dc85efd95ba11cc3a6871d411f6c29ab1961d637
Simon Boylan <siboy73@gmail.com>
2024-09-25 19:21:54 +0100

    Add stateful functions

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,16 +1,26 @@
 // import logo from './logo.svg';
-// import './App.css';
-import './App.module.css';
+
+import './App.css';
+// import './App.module.css';
+
 // import FunctionalGreeting from './components/FunctionalGreeting';
 // import FunctionalGreetingWithProps from './components/FunctionalGreetingWithProps';
-import Sidebar from './components/Sidebar';
+
+// import StatefulGreeting from './components/StatefulGreeting';
+// import StatefulGreetingWithCallback from './components/StatefulGreetingWithCallback';
+import StatefulGreetingWithPrevState from './components/StatefulGreetingWithPrevState';
+
+// import Sidebar from './components/Sidebar';
 
 function App() {
   return (
     <div className="App">
       {/* <FunctionalGreeting />
       <FunctionalGreetingWithProps greeting="Good Morning" name="Bob Smith" age="33" location="London" /> */}
-      <Sidebar />
+      {/* <Sidebar /> */}
+      {/* <StatefulGreeting greeting = "Good Morning" name = "Bob Smith" /> */}
+      {/* <StatefulGreetingWithCallback greeting = "Good Morning" name = "Bob Smith" /> */}
+      <StatefulGreetingWithPrevState greeting = "Good Morning" name = "Bob Smith" />
     </div>
   );
 }
