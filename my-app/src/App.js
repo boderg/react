01928a5a93355664c68c6cba7d537a6ba6706ecc
Simon Boylan <siboy73@gmail.com>
2024-09-26 11:30:36 +0100

    Comment out learning imports

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,26 +1,26 @@
 // import logo from './logo.svg';
 
-import './App.css';
-// import './App.module.css';
+// import './App.css';
+import './App.module.css';
 
 // import FunctionalGreeting from './components/FunctionalGreeting';
 // import FunctionalGreetingWithProps from './components/FunctionalGreetingWithProps';
 
 // import StatefulGreeting from './components/StatefulGreeting';
 // import StatefulGreetingWithCallback from './components/StatefulGreetingWithCallback';
-import StatefulGreetingWithPrevState from './components/StatefulGreetingWithPrevState';
+// import StatefulGreetingWithPrevState from './components/StatefulGreetingWithPrevState';
 
-// import Sidebar from './components/Sidebar';
+import Sidebar from './components/Sidebar';
 
 function App() {
   return (
     <div className="App">
-      {/* <FunctionalGreeting />
-      <FunctionalGreetingWithProps greeting="Good Morning" name="Bob Smith" age="33" location="London" /> */}
-      {/* <Sidebar /> */}
+      {/* <FunctionalGreeting /> */}
+      {/* <FunctionalGreetingWithProps greeting="Good Morning" name="Bob Smith" age="33" location="London" /> */}
       {/* <StatefulGreeting greeting = "Good Morning" name = "Bob Smith" /> */}
       {/* <StatefulGreetingWithCallback greeting = "Good Morning" name = "Bob Smith" /> */}
-      <StatefulGreetingWithPrevState greeting = "Good Morning" name = "Bob Smith" />
+      {/* <StatefulGreetingWithPrevState greeting = "Good Morning" name = "Bob Smith" /> */}
+      <Sidebar />
     </div>
   );
 }
