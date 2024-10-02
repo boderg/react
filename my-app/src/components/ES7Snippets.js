2bc677245bb96cfdb3dcabb91fa65aa1ecb8c65f
Simon Boylan <siboy73@gmail.com>
2024-10-02 12:39:23 +0100

    ES7 snippets learning

---
+++ my-app/src/components/ES7Snippets.js
@@ -0,0 +1,49 @@
+// rfce + tab creates a functional component as below
+// import React from 'react'
+
+// function ES7Snippets() {
+//     return (
+//         <div>ES7Snippets</div>
+//     )
+// }
+
+// export default ES7Snippets
+
+// rafce + tab creates a functional arrow component as below
+// import React from 'react'
+
+// const ES7Snippets = () => {
+//     return (
+//         <div>ES7Snippets</div>
+//     )
+// }
+
+// export default ES7Snippets
+
+// rce + tab creates a class component as below
+// import React, { Component } from 'react'
+
+// export class ES7Snippets extends Component {
+//     render() {
+//         return (
+//         <div>ES7Snippets</div>
+//         )
+//     }
+// }
+
+// export default ES7Snippets
+
+// rconst + tab creates a constructor component as below
+// constructor(props) {
+//     super(props)
+
+//     this.state = {
+//         
+//     }
+// }
+
+// clg + tab creates a console log component as below
+// console.log(object)
+
+// clo + tab creates a console log component and object name as a string as below
+// console.log('object', object)
